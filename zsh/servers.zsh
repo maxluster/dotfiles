f0efdb374ed2e01aacce2b4e1d8b0bf55d4d25bb
maxluster <maxluster@gmail.com>
2014-11-17 23:59:53 -0800

    servers

---
+++ zsh/servers.zsh
@@ -0,0 +1,14 @@
+bugs(){
+  cd $HOME/bugsnag/bugsnag-website
+  bundle exec rails s
+}
+
+bugc(){
+  cd $HOME/bugsnag/bugsnag-website
+  bundle exec rails c
+}
+
+maxs(){
+  cd $HOME/projects/maxluster
+  bundle exec middleman
+}
